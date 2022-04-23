755a9c6e80fd41f8793fa4ff8b2c16a0a4e6eeca
Silvaney De Sousa <silvaney.sousad@gmail.com>
2022-04-23 16:46:35 -0300

    origin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import{FiSearch} from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text" 
+        placeholder="Digite o CEP..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
     </div>
   );
 }
